fa28ed820f5ebe79604831f5c5f56e11c1e398c1
Nukr <nukrs.w@gmail.com>
2016-10-09 07:21:27 +0800

    move funcs to dal-go

--- main.go
+++ main.go
@@ -13,15 +13,14 @@ func main() {
 	username := chaos.Alphabet(12)
 	password := chaos.Alphabet(12)
 	fmt.Printf("username: %s, password %s\n", username, password)
-	accessToken := createAccount(username, password)
+	accessToken := dal.CreateAccount(username, password)
 	fmt.Printf("accessToken %s\n", accessToken)
-	serviceName := createService(chaos.Alphabet(15), accessToken)
+	serviceName := dal.CreateService(chaos.Alphabet(15), accessToken)
 	fmt.Printf("serviceName %s\n", serviceName)
 	tableName := chaos.Alphabet(15)
-	dbName := createTable(tableName, accessToken, serviceName)
+	dbName := dal.CreateTable(tableName, accessToken, serviceName)
 	fmt.Printf("tableName %s\n", tableName)
 	fmt.Printf("dbName %s\n", dbName)
-	fmt.Printf("total cost %s\n", time.Since(start))
 	d := dal.DAL{
 		URL:         "http://localhost:12345/graphql",
 		AccessToken: accessToken,
@@ -35,4 +34,5 @@ func main() {
 
 	result := d.CreateObject(tableName, s)
 	fmt.Println(result)
+	fmt.Printf("total cost %s\n", time.Since(start))
 }
